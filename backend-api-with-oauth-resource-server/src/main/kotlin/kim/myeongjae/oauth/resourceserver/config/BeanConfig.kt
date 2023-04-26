7f189177f4156f9fc37b43bb580a5de4a35b9e27
김명재 Myeongjae Kim <dev@myeongjae.kim>
2023-04-26 19:24:16 +0900

    fix: fix import

--- backend-api-with-oauth-resource-server/src/main/kotlin/kim/myeongjae/oauth/resourceserver/config/BeanConfig.kt
+++ backend-api-with-oauth-resource-server/src/main/kotlin/kim/myeongjae/oauth/resourceserver/config/BeanConfig.kt
@@ -6,7 +6,13 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator
 import org.springframework.security.oauth2.core.OAuth2TokenIntrospectionClaimNames.AUD
 import org.springframework.security.oauth2.core.OAuth2TokenValidator
-import org.springframework.security.oauth2.jwt.*
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.jwt.JwtClaimValidator
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.JwtDecoders
+import org.springframework.security.oauth2.jwt.JwtIssuerValidator
+import org.springframework.security.oauth2.jwt.JwtTimestampValidator
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
 import java.time.Duration
 import java.time.temporal.ChronoUnit
 
